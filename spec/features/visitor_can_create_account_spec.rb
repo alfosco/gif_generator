95df6f8184844cfc28163447fdc0be8b2c824b9d
Alex Fosco <alex.hf88@gmail.com>
2017-02-16 22:42:38 -0700

    initial commit

---
+++ spec/features/visitor_can_create_account_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor tries to create account" do
+  context "provides valid credentials" do
+    scenario "account creation is successful" do
+      visit sign_up_path
+
+      fill_in "Username", with: "NewUser"
+      fill_in "Email", with: "NewUser@example.com"
+      fill_in "Password", with: "password"
+      fill_in "Password confirmation", with: "password"
+      click_on "Create Account"
+
+      expect(current_path).to eq(user_path(User.last))
+
+      within("h1.greeting") do
+        expect(page).to have_content("Welcome NewUser!")
+      end
+      
+    end
+  end
+end
